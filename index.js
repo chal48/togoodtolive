66924d773f83461949100e9b7873a3392a6638d5
BoBynou <90771596+BoBynou@users.noreply.github.com>
2023-04-13 18:07:35 +0200

    test

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 8000
+app.use('/css', express.static(__dirname + '/public/style/style.css'));
 
 app.get('/api', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/template/home.html')
 })
 
 app.listen(port, () => {
