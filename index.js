a160230cf0b72c67c30b6dd299acef64329957a6
emma <emma.picard@ynov.com>
2023-04-13 17:55:12 +0200

    add profil page

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+app.use('/css',express.static(__dirname+'/profil.css'))
+
 app.get('/api', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/user/:userId', (req, res) => {
+    res.send(req.params.userId)
+})
+
+app.get('/profil', (req, res) => {
+    res.sendFile(__dirname+'/profil.html')
 })
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}/api`)
 })
+
+
