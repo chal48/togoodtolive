92fad73389df7ee4380624b65811b901a515970b
GamesDarts <leo39300.alves@gmail.com>
2023-04-14 09:24:57 +0200

    début page

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 8001
+
+app.use("/css", express.static(__dirname+"/public/style/login.css"))
 
 app.get('/api', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!nkcsejhbc')
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname+"/public/template/login.html")
 })
 
 app.listen(port, () => {
