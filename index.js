366fe69657ec808d5e5159bd34c941ff7ecf656d
GamesDarts <leo39300.alves@gmail.com>
2023-04-14 15:10:39 +0200

    page login

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ const express = require('express')
 const app = express()
 const port = 8001
 
-app.use("/css", express.static(__dirname+"/public/style/login.css"))
+app.use('/css/:filename', (req, res) =>{
+    var filename = req.params.filename;
+    res.sendFile(__dirname + '/public/style/' + filename);
+});
+
+app.use('/img/:filename', (req, res) =>{
+    var filename = req.params.filename;
+    res.sendFile(__dirname + '/public/img/' + filename);
+});
+
+app.use('/js/:filename', (req, res) =>{
+    var filename = req.params.filename;
+    res.sendFile(__dirname + '/public/script/' + filename);
+});
+
 
 app.get('/api', (req, res) => {
     res.send('Hello World!nkcsejhbc')
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname+"/public/template/login.html")
+    res.sendFile('/template/login.html', {root: './public' })
+})
+
+app.get('/create', (req, res) => {
+    res.sendFile('/template/create.html', {root: './public' })
 })
 
 app.listen(port, () => {
