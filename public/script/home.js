ae173a070e5a844212df0577edf57b065ee376e4
Antoine GUERIN <antoine.guerin@ynov.com>
2023-04-15 03:30:01 +0200

    feat: start to add form to create post

--- public/script/home.js
+++ public/script/home.js
@@ -5,27 +5,27 @@ xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
         let allPosts = JSON.parse(xhr.responseText)
         if (allPosts.length == 0) {
-            document.getElementsByTagName('main')[0].insertAdjacentHTML('beforeend', '<p class="no-posts">Pas de posts, revenez plus tard !</p>')
+            document.getElementsByTagName('main')[0].insertAdjacentHTML('beforeend', '<div class="no-post-store"><h2 class="no-posts">Pas de posts, revenez plus tard !</h2></div>')
         }else{
             allPosts.forEach(element => {
                 let startDate = new Date(element.start_date)
                 let endDate = new Date(element.end_date)
-    
+
                 let startDateString = ((startDate.getDate() < 10) ? '0' + (startDate.getDate() + 1) : startDate.getDate() + 1) + '/' + ((startDate.getMonth() < 10) ? '0' + (startDate.getMonth() + 1) : startDate.getMonth() + 1) + '/' + startDate.getFullYear()
                 let endDateString = ((endDate.getDate() < 10) ? '0' + (endDate.getDate()) : endDate.getDate()) + '/' + ((endDate.getMonth() < 10) ? '0' + (endDate.getMonth() + 1) : endDate.getMonth() + 1) + '/' + endDate.getFullYear()
-    
+
                 let divConainerString = document.getElementById("div0").innerHTML;
-    
+
                 divConainerString = divConainerString.replaceAll('{{title}}', element.title)
                 divConainerString = divConainerString.replaceAll('{{content}}', element.content)
                 divConainerString = divConainerString.replaceAll('{{dateStart}}', startDateString)
                 divConainerString = divConainerString.replaceAll('{{dateEnd}}', endDateString)
-    
+
                 getUserById(element.user_id)
                 .then((email)=>{
                     divConainerString = divConainerString.replaceAll('{{userEmail}}', email)
                     divConainerString = '<div class="carré">' + divConainerString +'</div>'
-        
+
                     document.getElementsByTagName('main')[0].insertAdjacentHTML('beforeend', divConainerString)
                 })
             });
@@ -33,6 +33,15 @@ xhr.onreadystatechange = function() {
     }
 };
 xhr.send();
+
+window.onload = ()=>{
+    let form = document.getElementById('add-post')
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        const formData = new FormData(form);
+        console.log(formData.get('title'))
+    })
+}
 
 function openPopup() {
     document.getElementById("popup").style.display = "block";
